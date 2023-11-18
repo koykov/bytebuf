29ee0975396eda03d94b88599826308426c0ebac
Konstantin Voykov <koykov0@gmail.com>
2023-11-18 13:07:55 +0200

    Extend AccBufWriter: add extra IO methods.

--- accbuf_writer.go
+++ accbuf_writer.go
@@ -9,3 +9,13 @@ func (b AccBufWriter) Write(p []byte) (int, error) {
 	b.AccBuf.Write(p)
 	return len(p), nil
 }
+
+func (b AccBufWriter) WriteByte(p byte) error {
+	b.AccBuf.WriteByte(p)
+	return nil
+}
+
+func (b AccBufWriter) WriteString(s string) (int, error) {
+	b.AccBuf.WriteStr(s)
+	return len(s), nil
+}
