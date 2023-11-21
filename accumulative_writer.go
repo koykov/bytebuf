0d3eb2f57cbdc36dd0432a84c93a0b683eb69d49
Konstantin Voykov <koykov0@gmail.com>
2023-11-21 22:54:37 +0200

    Extend AccumulativeWriter with extra methods, similar to Accumulative buffer.

--- accumulative_writer.go
+++ accumulative_writer.go
@@ -5,6 +5,14 @@ type AccumulativeWriter struct {
 	buf *Accumulative
 }
 
+func (b AccumulativeWriter) Bytes() []byte {
+	return b.buf.Bytes()
+}
+
+func (b AccumulativeWriter) String() string {
+	return b.buf.String()
+}
+
 func (b AccumulativeWriter) Write(p []byte) (int, error) {
 	b.buf.Write(p)
 	return len(p), nil
@@ -18,4 +26,64 @@ func (b AccumulativeWriter) WriteByte(p byte) error {
 func (b AccumulativeWriter) WriteString(s string) (int, error) {
 	b.buf.WriteString(s)
 	return len(s), nil
+}
+
+func (b AccumulativeWriter) WriteInt(i int64) (int, error) {
+	off := b.buf.Len()
+	b.buf.WriteInt(i)
+	return b.buf.Len() - off, nil
+}
+
+func (b AccumulativeWriter) WriteUint(u uint64) (int, error) {
+	off := b.buf.Len()
+	b.buf.WriteUint(u)
+	return b.buf.Len() - off, nil
+}
+
+func (b AccumulativeWriter) WriteFloat(f float64) (int, error) {
+	off := b.buf.Len()
+	b.buf.WriteFloat(f)
+	return b.buf.Len() - off, nil
+}
+
+func (b AccumulativeWriter) WriteBool(b_ bool) (int, error) {
+	off := b.buf.Len()
+	b.buf.WriteBool(b_)
+	return b.buf.Len() - off, nil
+}
+
+func (b AccumulativeWriter) WriteX(x any) (int, error) {
+	off := b.buf.Len()
+	b.buf.WriteX(x)
+	return b.buf.Len() - off, b.buf.Error()
+}
+
+func (b AccumulativeWriter) WriteMarshallerTo(m MarshallerTo) (int, error) {
+	off := b.buf.Len()
+	b.buf.WriteMarshallerTo(m)
+	return b.buf.Len() - off, nil
+}
+
+func (b AccumulativeWriter) WriteApplyFn(p []byte, fn func(dst, p []byte) []byte) (int, error) {
+	off := b.buf.Len()
+	b.buf.WriteApplyFn(p, fn)
+	return b.buf.Len() - off, nil
+}
+
+func (b AccumulativeWriter) WriteApplyFnString(s string, fn func(dst, p []byte) []byte) (int, error) {
+	off := b.buf.Len()
+	b.buf.WriteApplyFnString(s, fn)
+	return b.buf.Len() - off, nil
+}
+
+func (b AccumulativeWriter) Len() int {
+	return b.buf.Len()
+}
+
+func (b AccumulativeWriter) Cap() int {
+	return b.buf.Cap()
+}
+
+func (b AccumulativeWriter) Reset() {
+	b.buf.Reset()
 }
